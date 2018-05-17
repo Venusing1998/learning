2aad30b1bf4cf8a62a02cbdf80d7456383effba1
venusing1998 <ago2chris@163.com>
2018-05-17 14:30:17 +0800

    ok

--- c++/3.1.cpp
+++ c++/3.1.cpp
@@ -3,13 +3,15 @@ using namespace std;
 
 int main()
 {
-	int value = 2;
-	int pow = 10;
-	int result = 1;
-	for (int cnt = 0; cnt != pow; ++cnt)
+	int base, exponent, result = 1;
+	cout << "Enter base: " << endl;
+	cin >> base;
+	cout << "Enter exponent: " << endl;
+	cin >> exponent;
+	for (int cnt = 0; cnt != exponent; ++cnt)
 	{
-		result *= value;
+		result *= base;
 	}
-	cout << value << " raised to the pow of " << pow << ":" << result << endl;
+	cout << base << " raised to the power of " << exponent << ": " << result << endl;
 	return 0;
 }
--- c++/test.cpp
+++ c++/test.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
+using namespace std;
 
 int gcd(int v1, int v2)
 {
 	while (v2)
-		{
-			int temp = v2;
-			v2 = v1 % v2;
-			v1 = temp;
-		}
+	{
+		int temp = v2;
+		v2 = v1 % v2;
+		v1 = temp;
+	}
 	return v1;
 }
 
 int main()
 {
-	std::cout << "Enter two values: \n";
+	cout << "Enter two values: \n";
 	int i, j;
-	std::cin >> i >> j;
-	std::cout << "gcd: " << gcd(i, j) << std::endl;
+	cin >> i >> j;
+	cout << "gcd: " << gcd(i, j) << endl;
 }
