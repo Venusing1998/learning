c79f4a400674383b817fb6eca42463dd997ed08a
venusing1998 <ago2chris@163.com>
2018-05-17 14:08:49 +0800

    ok

--- c++/test.cpp
+++ c++/test.cpp
@@ -1,3 +1,20 @@
 #include <iostream>
-using spacename std;
 
+int gcd(int v1, int v2)
+{
+	while (v2)
+		{
+			int temp = v2;
+			v2 = v1 % v2;
+			v1 = temp;
+		}
+	return v1;
+}
+
+int main()
+{
+	std::cout << "Enter two values: \n";
+	int i, j;
+	std::cin >> i >> j;
+	std::cout << "gcd: " << gcd(i, j) << std::endl;
+}
