4040be2f78a996235c564938911c0e2da525faa9
venusing1998 <ago2chris@163.com>
2018-05-18 09:39:40 +0800

    ok

--- c++/test.cpp
+++ c++/test.cpp
@@ -1,21 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int gcd(int v1, int v2)
-{
-	while (v2)
-	{
-		int temp = v2;
-		v2 = v1 % v2;
-		v1 = temp;
-	}
-	return v1;
-}
-
 int main()
 {
-	cout << "Enter two values: \n";
-	int i, j;
-	cin >> i >> j;
-	cout << "gcd: " << gcd(i, j) << endl;
+	string s;
+	cout << "Enter a string:" << endl;
+	cin >> s;
+	cout << s << endl;
+    return 0;
 }
